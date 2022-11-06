7ad05fb5395327b13bcd407f3429b769971b4837
HOME-DESKTOP <baloonflower554@gmail.com>
2022-11-06 19:02:29 +0900

    디자인 수정 완료

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,17 @@
 export function getAllTags(posts) {
-    const allTags = new Set();
+    const allTags = new Set()
     posts.forEach((post) => {
-        post.frontmatter.tags?.map((tag) => allTags.add(tag.toLowerCase()));
-    });
-    return [...allTags];
+        post.frontmatter.tags?.map((tag) => allTags.add(tag.toLowerCase()))
+    })
+    return [...allTags]
+}
+
+export function getAllTagsWithCount(posts) {
+    return posts.reduce((prev, post) => {
+        const currTags = { ...prev }
+        post.frontmatter.tags?.forEach(function (tag) {
+            currTags[tag] = (currTags[tag] || 0) + 1
+        })
+        return currTags
+    }, {})
 }
